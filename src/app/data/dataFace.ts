0398a38b893ddb77f6ee25e0dc18a5202299b4de
IcaroRP <icaroribeiroam@gmail.com>
2022-11-28 16:19:44 -0400

    aggregating css and decoupling main page components

---
+++ src/app/data/dataFace.ts
@@ -0,0 +1,15 @@
+export const dataFake = [
+  {
+    "id":1,
+    "photo":"link",
+    "title":"noticia",
+    "description":"a descricao"
+  },
+  {
+    "id":2,
+    "photo":"link",
+    "title":"noticia",
+    "description":"a descricao"
+  }
+]
+
---
+++ src/app/pages/content/content.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-content',
+  templateUrl: './content.component.html',
+  styleUrls: ['./content.component.css']
+})
+export class ContentComponent implements OnInit {
+  photoCover:string = "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Placeholder_view_vector.svg/681px-Placeholder_view_vector.svg.png"
+  contentTitle:string = "ARTIGO"
+  contentDescription:string = "Conteudo do Artigo"
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
