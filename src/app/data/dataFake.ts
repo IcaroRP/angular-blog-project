97b071d2ec66b4e542c1d0982bbf9700acb51882
IcaroRP <icaroribeiroam@gmail.com>
2022-11-28 16:59:15 -0400

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,15 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title":"Noticia Placeholder",
+    "description":"Descricao Placeholder",
+    "photoCover":"https://www.unfe.org/wp-content/uploads/2019/04/SM-placeholder.png"
+  },
+  {
+    "id":"2",
+    "title":"Outra Noticia",
+    "description":"A descricao",
+    "photoCover":"https://www.unfe.org/wp-content/uploads/2019/04/SM-placeholder.png"
+  }
+]
+
