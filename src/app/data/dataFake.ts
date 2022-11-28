be365fbbce4108a630c7ba93cf11b33e9b050204
IcaroRP <icaroribeiroam@gmail.com>
2022-11-28 18:55:37 -0400

    new header

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,14 +1,26 @@
 export const dataFake = [
   {
     "id":"1",
-    "title":"Noticia Placeholder",
-    "description":"Descricao Placeholder",
+    "title":"Estudante de programação descobre magia do placeholder",
+    "description":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras in lacus eget ligula auctor mattis. Etiam maximus fermentum odio, sed aliquet arcu pharetra in. In id urna non tortor venenatis imperdiet. Vivamus rhoncus varius leo eget maximus. Integer pretium elit a neque malesuada porta. Duis imperdiet ultricies nisl eu finibus. Vivamus ullamcorper tellus mauris, eu ullamcorper est eleifend iaculis. Donec in nisl auctor, malesuada quam eu, faucibus enim. Aliquam in faucibus lacus. Aliquam facilisis tortor metus, et semper arcu imperdiet id. Duis pulvinar rutrum mauris, quis eleifend elit rutrum id.",
     "photoCover":"https://www.unfe.org/wp-content/uploads/2019/04/SM-placeholder.png"
   },
   {
     "id":"2",
-    "title":"Outra Noticia",
-    "description":"A descricao",
+    "title":"Como criar infalível? Aprenda agora a forma correta!",
+    "description":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras in lacus eget ligula auctor mattis. Etiam maximus fermentum odio, sed aliquet arcu pharetra in. In id urna non tortor venenatis imperdiet. Vivamus rhoncus varius leo eget maximus. Integer pretium elit a neque malesuada porta. Duis imperdiet ultricies nisl eu finibus. Vivamus ullamcorper tellus mauris, eu ullamcorper est eleifend iaculis. Donec in nisl auctor, malesuada quam eu, faucibus enim. Aliquam in faucibus lacus. Aliquam facilisis tortor metus, et semper arcu imperdiet id. Duis pulvinar rutrum mauris, quis eleifend elit rutrum id.",
+    "photoCover":"https://www.unfe.org/wp-content/uploads/2019/04/SM-placeholder.png"
+  },
+  {
+    "id":"3",
+    "title":"3 opções erradas para dinheiro",
+    "description":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras in lacus eget ligula auctor mattis. Etiam maximus fermentum odio, sed aliquet arcu pharetra in. In id urna non tortor venenatis imperdiet. Vivamus rhoncus varius leo eget maximus. Integer pretium elit a neque malesuada porta. Duis imperdiet ultricies nisl eu finibus. Vivamus ullamcorper tellus mauris, eu ullamcorper est eleifend iaculis. Donec in nisl auctor, malesuada quam eu, faucibus enim. Aliquam in faucibus lacus. Aliquam facilisis tortor metus, et semper arcu imperdiet id. Duis pulvinar rutrum mauris, quis eleifend elit rutrum id.",
+    "photoCover":"https://www.unfe.org/wp-content/uploads/2019/04/SM-placeholder.png"
+  },
+  {
+    "id":"4",
+    "title":"Não jogue fora seu dinheiro",
+    "description":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras in lacus eget ligula auctor mattis. Etiam maximus fermentum odio, sed aliquet arcu pharetra in. In id urna non tortor venenatis imperdiet. Vivamus rhoncus varius leo eget maximus. Integer pretium elit a neque malesuada porta. Duis imperdiet ultricies nisl eu finibus. Vivamus ullamcorper tellus mauris, eu ullamcorper est eleifend iaculis. Donec in nisl auctor, malesuada quam eu, faucibus enim. Aliquam in faucibus lacus. Aliquam facilisis tortor metus, et semper arcu imperdiet id. Duis pulvinar rutrum mauris, quis eleifend elit rutrum id.",
     "photoCover":"https://www.unfe.org/wp-content/uploads/2019/04/SM-placeholder.png"
   }
 ]
