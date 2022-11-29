b169614757895f1a2385c0563951e1aa117fe71a
IcaroRP <icaroribeiroam@gmail.com>
2022-11-29 15:34:54 -0400

    tip of the day of publication and modification of the content area

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -3,7 +3,7 @@ export const dataFake = [
     "id":"1",
     "title":"Estudante de programação descobre magia do placeholder",
     "description":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras in lacus eget ligula auctor mattis. Etiam maximus fermentum odio, sed aliquet arcu pharetra in. In id urna non tortor venenatis imperdiet. Vivamus rhoncus varius leo eget maximus. Integer pretium elit a neque malesuada porta. Duis imperdiet ultricies nisl eu finibus. Vivamus ullamcorper tellus mauris, eu ullamcorper est eleifend iaculis. Donec in nisl auctor, malesuada quam eu, faucibus enim. Aliquam in faucibus lacus. Aliquam facilisis tortor metus, et semper arcu imperdiet id. Duis pulvinar rutrum mauris, quis eleifend elit rutrum id.",
-    "photoCover":"https://www.unfe.org/wp-content/uploads/2019/04/SM-placeholder.png"
+    "photoCover":"https://www.unfe.org/wp-content/uploads/2019/04/SM-placeholder.png",
   },
   {
     "id":"2",
